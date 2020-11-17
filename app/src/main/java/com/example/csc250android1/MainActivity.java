0ff981ff0f095a74cda5da6b23d02487650d5e10
mlitman <awesomefat@gmail.com>
2020-11-17 12:48:57 -0600

    made changes to show MVC

--- app/src/main/java/com/example/csc250android1/MainActivity.java
+++ app/src/main/java/com/example/csc250android1/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.csc250android1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v)
+    {
+        TextView myTextView = this.findViewById(R.id.myTextView);
+        myTextView.setText("Hello World!!!");
     }
 }
